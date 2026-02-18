bb2203d860794d77ba31bd2f1c0eb519f11bcd8b
Daly <bouazizimedali99@gmail.com>
2026-02-18 14:35:01 +0100

    halfway to complete subject list page

--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {ListView} from "@/components/refine-ui/views/list-view.tsx";
+import {Breadcrumb} from "@/components/ui/breadcrumb.tsx";
+import {Input} from "@/components/ui/input.tsx";
 
 const SubjectList = () => {
+   const [searchQuery, setSearchQuery] = useState("");
     return (
-        <div>SubjectList</div>
+
+        <ListView>
+            <Breadcrumb/>
+                <h1 className="Page-title"> Subjects</h1>
+            <div className="intro-row">
+                <p> Quick access to essential metric and management tools.</p>
+                <div className="actions-row">
+                    <div className="search-field">
+                        <search className="search-icon"/>
+                        <Input
+                        type="Text"
+                        placeholder = "Search"
+                        className = "pl-10 w-full"
+                        value={searchQuery}
+                        ></Input>
+
+                    </div>
+                </div>
+            </div>
+        </ListView>
     )
 }
 export default SubjectList
